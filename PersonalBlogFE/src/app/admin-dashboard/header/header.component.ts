cd302385f70fe8b64a4d95770c5608ab04edc6c7
CLJR <nclam.work.2002@gmail.com>
2025-06-11 23:42:23 +0700

    FE - Add Header for admin site

---
+++ PersonalBlogFE/src/app/admin-dashboard/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  public darkMode: boolean = false;
+
+  constructor() {
+    // Initialization logic can go here if needed
+  }
+
+  toggleDarkMode(): void {
+    this.darkMode = !this.darkMode;
+  }
+
+}
--- PersonalBlogFE/src/app/app.module.ts
+++ PersonalBlogFE/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { HeaderComponent } from './admin-dashboard/header/header.component';
+import { SidebarComponent } from './admin-dashboard/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
